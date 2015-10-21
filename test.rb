ba11457558301a1d728ee207df49fb67f472c4e4
natalya.patrikeeva <natalya.patrikeeva@gmail.com>
2015-10-20 20:31:08 -0500

    test commited

--- test.rb
+++ test.rb
@@ -1,62 +1,6 @@
-class Blog
-  @@num_posts = 0
-  @@the_blog = []
+require 'nokogiri'
+require 'open-uri'
 
-  def get_input(input_str)
-    puts input_str
-    return_value = gets.chomp
-  end
-
-  def publish_blog
-    @@the_blog.each do |thehash|
-      puts "\nTitle: #{thehash.fetch(:title)}"
-      puts "Entry: #{thehash.fetch(:content)} on #{thehash.fetch(:p_date)}"
-    end
-  end
-
-  def test_user(test_str, true_val)
-    puts test_str
-    gets.downcase.chomp == true_val ? true : false
-  end
-
-  def save_blog (blog_hash)
-    @@the_blog[@@num_posts] = blog_hash
-    @@num_posts += 1
-    test_user('Would you like to make another entry? Y/N', 'y') ? runblog : publish_blog
-    puts "the class of blog_hash is: " + blog_hash.class.to_s
-  end
-
-  def self.current_count
-		puts "There are currently #{@@num_posts} instances of my Blog class."
-	end
-end
-
-class BlogPost < Blog
-  attr_accessor :author, :title, :content, :pub_date, :blog_post
-
-  def initialize
-    @author = get_input ("What is your name?")
-    @title = get_input("Enter your Blog Title:")
-    @content = get_input("Enter your blog post now:")
-    @pub_date = Time.now.to_s
-      5.times do @pub_date.chop! end
-
-    @blog_post = {
-      title: @title,
-      content: @content,
-      p_date: @pub_date,
-      author_name: @author
-    }
-    self.save_blog(@blog_post)
-  end
-end
-
-def runblog
-  my_blog = BlogPost.new
-
-  BlogPost.current_count
-  puts BlogPost.inspect
-  puts my_blog.inspect
-end
-
-runblog
+doc = Nokogiri::HTML(open('http://www.wikipedia.org'))
+#doc = Nokogiri::HTML(open('http://www.google.com'))
+puts doc
